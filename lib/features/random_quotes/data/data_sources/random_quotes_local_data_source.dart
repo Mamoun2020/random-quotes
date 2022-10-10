936c341509aaee625a34868bf4a2141e5f42bba2
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-10 10:53:34 +0300

    local data source and repository and add network info

--- lib/features/random_quotes/data/data_sources/random_quotes_local_data_source.dart
+++ lib/features/random_quotes/data/data_sources/random_quotes_local_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:one_studio_core/core.dart';
 import 'package:quotes/features/random_quotes/data/models/quote.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -7,21 +8,21 @@ class RandomQuotesLocalDataSource {
   final SharedPreferences _sharedPreference;
 
   RandomQuotesLocalDataSource(this._sharedPreference);
+
   static var cachedRandomQuote = 'CACHED_RANDOM_QUOTE';
 
-  Future<Quote> getLastRandomQuote() {
-    final quotes = _sharedPreference.getString(RandomQuotesLocalDataSource.cachedRandomQuote);
+  SingleResponse<Quote> getLastRandomQuote() {
+    final quotes = _sharedPreference
+        .getString(RandomQuotesLocalDataSource.cachedRandomQuote);
 
-    if (quotes != null) {
-      final cacheRandomQuote =
-          Future.value(Quote.fromJson(json.decode(quotes)));
-      return cacheRandomQuote;
-    } else {
-      throw Exception();
+    if (quotes == null) {
+      throw CacheException(errors: {'CacheException':'something is wrong'});
     }
+    return SingleResponse<Quote>.fromJson(json.decode(quotes));
   }
 
-  Future<void> cacheQuote(Quote quote) {
-    return _sharedPreference.setString(RandomQuotesLocalDataSource.cachedRandomQuote, json.encode(quote));
+  Future<void> cacheQuote(SingleResponse<Quote> quote) {
+    return _sharedPreference.setString(
+        RandomQuotesLocalDataSource.cachedRandomQuote, json.encode(quote));
   }
 }
