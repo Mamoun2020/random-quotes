77ee5f0ad0ff87cb64760326a6a124dd03ebae3e
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-10 11:27:09 +0300

    logic holder... Cubit

--- lib/features/random_quotes/presentation/cubit/random_quotes_cubit.dart
+++ lib/features/random_quotes/presentation/cubit/random_quotes_cubit.dart
@@ -1,8 +1,32 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:one_studio_core/core.dart';
+import 'package:quotes/features/random_quotes/data/models/quote.dart';
+import 'package:quotes/features/random_quotes/data/repositories/random_quotes_repository.dart';
 
 part 'random_quotes_state.dart';
 
 class RandomQuotesCubit extends Cubit<RandomQuotesState> {
-  RandomQuotesCubit() : super(RandomQuotesInitial());
+  final RandomQuotesRepository _repository;
+  final Quote _quote;
+  RandomQuotesCubit(this._repository, this._quote)
+      : super(RandomQuotesInitial());
+
+  Future<void> getRandomQuote() async {
+    final response = await _repository.randomQuotes();
+    response.fold(
+        (failure) => emit(RandomQuotesError(msg: failure.toMessage())),
+        (quote) => emit(RandomQuotesLoaded(quote: quote)));
+  }
+
+  String _mapFailureToMsg(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return 'Server Failure';
+      case CacheFailure:
+        return 'Cache Failure';
+      default:
+        return 'Unexpected Error';
+    }
+  }
 }
--- lib/features/random_quotes/presentation/cubit/random_quotes_state.dart
+++ lib/features/random_quotes/presentation/cubit/random_quotes_state.dart
@@ -2,9 +2,24 @@ part of 'random_quotes_cubit.dart';
 
 abstract class RandomQuotesState extends Equatable {
   const RandomQuotesState();
-}
-
-class RandomQuotesInitial extends RandomQuotesState {
   @override
   List<Object> get props => [];
+}
+
+class RandomQuotesInitial extends RandomQuotesState {}
+
+class RandomQuotesIsLoading extends RandomQuotesState{}
+
+class RandomQuotesLoaded extends RandomQuotesState{
+  final SingleResponse<Quote> quote;
+  const RandomQuotesLoaded({required this.quote});
+  @override
+  List<Object> get props => [quote];
+}
+
+class RandomQuotesError extends RandomQuotesState{
+  final String msg;
+  const RandomQuotesError({required this.msg});
+  @override
+  List<Object> get props => [msg];
 }
