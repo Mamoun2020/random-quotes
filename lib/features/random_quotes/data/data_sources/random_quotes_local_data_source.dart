4875e8c73aaa2984b816bdf0661254000f4616b5
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-10 10:01:15 +0300

    local data source to cacheQuote and get last quote cached

--- lib/features/random_quotes/data/data_sources/random_quotes_local_data_source.dart
+++ lib/features/random_quotes/data/data_sources/random_quotes_local_data_source.dart
@@ -1,3 +1,27 @@
+import 'dart:convert';
 
+import 'package:quotes/features/random_quotes/data/models/quote.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class RandomQuotesLocalDataSource {
+  final SharedPreferences _sharedPreference;
 
+  RandomQuotesLocalDataSource(this._sharedPreference);
+  static var cachedRandomQuote = 'CACHED_RANDOM_QUOTE';
+
+  Future<Quote> getLastRandomQuote() {
+    final quotes = _sharedPreference.getString(RandomQuotesLocalDataSource.cachedRandomQuote);
+
+    if (quotes != null) {
+      final cacheRandomQuote =
+          Future.value(Quote.fromJson(json.decode(quotes)));
+      return cacheRandomQuote;
+    } else {
+      throw Exception();
+    }
+  }
+
+  Future<void> cacheQuote(Quote quote) {
+    return _sharedPreference.setString(RandomQuotesLocalDataSource.cachedRandomQuote, json.encode(quote));
+  }
+}
