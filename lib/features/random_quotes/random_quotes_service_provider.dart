db0f3d99b11bac2c12b6d969496ecee0376f6080
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-10 11:04:25 +0300

    service provider ... wip

--- lib/features/random_quotes/random_quotes_service_provider.dart
+++ lib/features/random_quotes/random_quotes_service_provider.dart
@@ -1,12 +1,27 @@
 
 import 'package:get_it/get_it.dart';
 import 'package:one_studio_core/core.dart';
+import 'package:quotes/features/random_quotes/data/data_sources/random_quotes_local_data_source.dart';
+import 'package:quotes/features/random_quotes/data/models/quote.dart';
+import 'package:quotes/features/random_quotes/data/repositories/random_quotes_repository.dart';
+
+import 'data/data_sources/random_quotes_remote_data_source.dart';
 
 class RandomQuotesServiceProvider extends ServiceProvider{
   @override
-  Future<void> register(GetIt it) {
-    // TODO: implement register
-    throw UnimplementedError();
+  Future<void> register(GetIt it) async{
+   it.registerFactory(() => RandomQuotesLocalDataSource(it()));
+   it.registerFactory(() => RandomQuotesRemoteDataSource(it()));
+   it.registerFactory(() => RandomQuotesRepository(it(),it(),it()));
   }
 
+  @override
+  T? responseConvert<T>(Map<String, dynamic> json) {
+    switch (T) {
+      case Quote:
+        return Quote.fromJson(json) as T;
+      default:
+        return null;
+    }
+  }
 }
