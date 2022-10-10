bc2f46bb64994caefd8928036a8b65fefe0951d2
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-10 08:49:16 +0300

    logic for get random quotes... wip

---
+++ lib/features/random_quotes/presentation/cubit/random_quotes_state.dart
@@ -0,0 +1,10 @@
+part of 'random_quotes_cubit.dart';
+
+abstract class RandomQuotesState extends Equatable {
+  const RandomQuotesState();
+}
+
+class RandomQuotesInitial extends RandomQuotesState {
+  @override
+  List<Object> get props => [];
+}
