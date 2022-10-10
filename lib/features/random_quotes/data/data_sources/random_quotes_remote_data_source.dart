ac977b761c4e128f50ebf589fb39895a4dc8451d
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-10 09:45:46 +0300

    remote data source

---
+++ lib/features/random_quotes/data/data_sources/random_quotes_remote_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:one_studio_core/core.dart';
+import 'package:quotes/features/random_quotes/data/models/quote.dart';
+import 'package:dio/dio.dart';
+
+class RandomQuotesRemoteDataSource extends DataSource{
+  final ApiClient _client;
+
+  RandomQuotesRemoteDataSource(this._client);
+
+  Future<SingleResponse<Quote>> quotes() async{
+    final Response response= await _client.get(RandomQuotesApis.quotes, headers: Api.headers());
+
+    return mapSingleResponse(response);
+  }
+
+}
+
+extension RandomQuotesApis on Api{
+  static String get quotes => 'random.json';
+}
